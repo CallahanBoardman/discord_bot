833c873f7b665c6505672480f54a2e8a4921bbfd
ItsMeBillFromLeftForDead <calboardman@eastlink.ca>
2024-03-18 16:21:35 -0300

    Initial commit

---
+++ discordbot.js
@@ -0,0 +1,21 @@
+// Initialize dotenv
+require('dotenv').config();
+
+// Discord.js versions ^13.0 require us to explicitly define client intents
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: 
+    [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.on('ready', () => {
+ console.log(`Logged in as ${client.user.tag}!`);
+});
+
+// Log In our bot
+client.login(process.env.CLIENT_TOKEN);
+
+client.on('messageCreate', msg => {
+    // You can view the msg object here with console.log(msg)
+     if (msg.content === 'Hello') {
+       msg.author.send(`冰淇淋 ${msg.author.username}`);
+     }
+    });
