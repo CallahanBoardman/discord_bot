79c242efbf2c01cca89255fe83008709aba801b5
ItsMeBillFromLeftForDead <calboardman@eastlink.ca>
2024-03-18 16:58:58 -0300

    Added the ability to do slash commands

--- discordbot.js
+++ discordbot.js
@@ -1,8 +1,7 @@
-// Initialize dotenv
-require('dotenv').config();
-
-// Discord.js versions ^13.0 require us to explicitly define client intents
-const { Client, GatewayIntentBits } = require('discord.js');
+const { clientId, guildId, token } = require('./config.json');
+const path = require('node:path');
+const fs = require('node:fs');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: 
     [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
@@ -10,8 +9,51 @@ client.on('ready', () => {
  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-// Log In our bot
-client.login(process.env.CLIENT_TOKEN);
+client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
+
+client.login(token);
 
 client.on('messageCreate', msg => {
     // You can view the msg object here with console.log(msg)
@@ -19,3 +61,5 @@ client.on('messageCreate', msg => {
        msg.author.send(`冰淇淋 ${msg.author.username}`);
      }
     });
+
+    
